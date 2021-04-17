10284c63bac71020423504ac8321a600e654e570
aisaenic <emilygao713@gmail.com>
2021-04-17 11:09:53 -0700

    removed locks, unneeded

--- body/buckets_script.py
+++ body/buckets_script.py
@@ -4,7 +4,7 @@ from iso3166 import countries
 import subprocess, threading, sys
 
 obj = Tranco(cache=False, cache_dir='.tranco')
-num_entries_per_piece = 5
+num_entries_per_piece = 20
 num_entries = int(sys.argv[1])
 lock = threading.Lock()
 #latest_date = date.today().strftime("%Y-%m-%d")
@@ -26,38 +26,38 @@ weird = []
 
 #latest_list.append('fail03.dnssec.works')
 def put_into_buckets(latest_list_piece: list):
-    with lock:
-        for current_domain in latest_list_piece:
-            results_for_domain = subprocess.run(["delv", dns_server, "+vtrace", "+multiline", current_domain], capture_output=True)
-            #print(results_for_domain.stderr.decode())
-            err = results_for_domain.stderr.decode()
-            out = results_for_domain.stdout.decode()
-            if 'fully validated' in out:
-                storage['fully_validated'].append(current_domain)
-            elif 'unsigned answer' in out:
-                storage['partially_validated'].append(current_domain)
-            elif 'SERVFAIL' in out or 'SERVFAIL' in err or 'resolution failed' in out or 'resolution failed' in err:
-                storage['not_supported'].append(current_domain)
-            else:
-                weird.append({  "domain": current_domain,
-                                "out": out,
-                                "debug_info": err})
-            domain_split = current_domain.split('.')
-            last_index = len(domain_split) - 1
-            tld = domain_split[last_index]
-            try:
-                tld = countries.get(tld).name
-            except KeyError:
-                dummy = False
-            if tld in countries_or_tlds:
-                countries_or_tlds[tld] += 1
-            else:
-                countries_or_tlds[tld] = 1
+    
+    for current_domain in latest_list_piece:
+        results_for_domain = subprocess.run(["delv", dns_server, "+vtrace", "+multiline", current_domain], capture_output=True)
+        #print(results_for_domain.stderr.decode())
+        err = results_for_domain.stderr.decode()
+        out = results_for_domain.stdout.decode()
+        if 'fully validated' in out:
+            storage['fully_validated'].append(current_domain)
+        elif 'unsigned answer' in out:
+            storage['partially_validated'].append(current_domain)
+        elif 'SERVFAIL' in out or 'SERVFAIL' in err or 'resolution failed' in out or 'resolution failed' in err:
+            storage['not_supported'].append(current_domain)
+        else:
+            weird.append({  "domain": current_domain,
+                            "out": out,
+                            "debug_info": err})
+        domain_split = current_domain.split('.')
+        last_index = len(domain_split) - 1
+        tld = domain_split[last_index]
+        try:
+            tld = countries.get(tld).name
+        except KeyError:
+            dummy = False
+        if tld in countries_or_tlds:
+            countries_or_tlds[tld] += 1
+        else:
+            countries_or_tlds[tld] = 1
 
 # needs to adjust for accuracy, first filter out generic TLDS then try country codes
 
 threads = []
-for i in range(5):
+for i in range(len(latest_list_pieces)):
     t = threading.Thread(target=put_into_buckets, args=[latest_list_pieces[i]])
     threads.append(t)
 
