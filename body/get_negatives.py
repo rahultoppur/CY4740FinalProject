e0b4454396aabdd6aefe912442563e5fdf166865
Emily Gao <emilygao713@gmail,com>
2021-04-21 13:07:09 -0400

    fixing get_negatives script

--- body/get_negatives.py
+++ body/get_negatives.py
@@ -3,10 +3,11 @@ import csv
 outfile = open("negatives.csv", "w")
 
 for i in range(11):
- data = open(f'./results_emily/{i:02}', 'r')
- latest = list(csv.reader(data))
- for entry in latest:
-  if "-2" == entry[2]:
-   outfile.write(str(entry) + "\n")
+ data = open(f'./results_emily/{i:02}', 'r').readlines()
+ #latest = list(csv.reader(data))
+ for i in range (len(data)):
+  data_as_list = data[i].split(',')
+  if "-2\n" == data_as_list[2]:
+   outfile.writelines(data[i])
 
 outfile.close()
