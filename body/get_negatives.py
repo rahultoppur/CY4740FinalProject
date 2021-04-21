aa562d016737117e689ab664c8342616a24288a6
Emily Gao <emilygao713@gmail,com>
2021-04-20 23:14:54 -0400

    adding script to get new input file from outputs with -2s

---
+++ body/get_negatives.py
@@ -0,0 +1,13 @@
+import csv
+
+
+outfile = open("negatives.csv", "w")
+
+for i in range(11):
+ data = open(f'./results_emily/{i:02}', 'r')
+ latest = list(csv.reader(data))
+ for entry in latest:
+  if "-2" == entry[2]:
+   outfile.write(str(entry) + "\n")
+
+outfile.close()
